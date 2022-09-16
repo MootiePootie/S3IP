907df0657ea418574c61d36d978d54dd4e985dd4
Cody Sommers <MootiePootie@outlook.com>
2022-09-16 15:15:41 -0700

    Add project files.

---
+++ TestProject/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace TestProject
+{
+    partial class S3IP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SelectImage = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.IpTxt = new System.Windows.Forms.TextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.DisconnectBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 118);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SelectImage
+            // 
+            this.SelectImage.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(324, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Print Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // IpTxt
+            // 
+            this.IpTxt.Location = new System.Drawing.Point(12, 125);
+            this.IpTxt.Name = "IpTxt";
+            this.IpTxt.Size = new System.Drawing.Size(100, 20);
+            this.IpTxt.TabIndex = 3;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(118, 124);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 4;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click_1);
+            // 
+            // DisconnectBtn
+            // 
+            this.DisconnectBtn.Enabled = false;
+            this.DisconnectBtn.Location = new System.Drawing.Point(199, 124);
+            this.DisconnectBtn.Name = "DisconnectBtn";
+            this.DisconnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.DisconnectBtn.TabIndex = 5;
+            this.DisconnectBtn.Text = "Disconnect";
+            this.DisconnectBtn.UseVisualStyleBackColor = true;
+            this.DisconnectBtn.Click += new System.EventHandler(this.DisconnectBtn_Click);
+            // 
+            // S3IP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 169);
+            this.Controls.Add(this.DisconnectBtn);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.IpTxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "S3IP";
+            this.Text = "S3IP";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog SelectImage;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox IpTxt;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button DisconnectBtn;
+    }
+}
+
---
+++ TestProject/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TestProject
+{
+    public partial class S3IP : Form
+    {
+
+        private static byte[] _buffer = new byte[1024];
+        private Socket _serverSocket;
+
+        void Connecter()
+        {
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            while (!_serverSocket.Connected)
+            {
+                try
+                {
+                    _serverSocket.Connect(IpTxt.Text, 6000);
+                    Connect.Enabled = false;
+                    DisconnectBtn.Enabled = true;
+                    button1.Enabled = true;
+                    Console.WriteLine("Connected to Nintendo Switch " + IpTxt.Text + "\r\n");
+                    if (pictureBox1.Image != null) 
+                    {
+                        button2.Enabled = true;
+                    }
+                }
+                catch (SocketException)
+                {
+                    MessageBox.Show("Failed To Connect");
+                    break;
+                }
+            }
+        }
+
+        void SendCmd(string cmd)
+        {
+            try
+            {
+                byte[] msg = Encoding.UTF8.GetBytes(cmd + "\r\n");
+                byte[] bytes = new byte[256];
+                int byteCount = _serverSocket.Send(msg, 0, msg.Length, SocketFlags.None);
+            }
+            catch (Exception ex)
+            {               
+                    MessageBox.Show("No Device Connected");                                           
+            }
+        }
+
+        
+
+        public S3IP()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelectImage.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+            DialogResult img = SelectImage.ShowDialog();
+            
+            if (img == DialogResult.OK) 
+            {
+
+                string file = SelectImage.FileName;
+                pictureBox1.Image = Image.FromFile(file);
+                if (Connect.Enabled == false) 
+                {
+                    button2.Enabled = true;
+                }
+               
+                
+            }
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                int x;
+                int y = 0;
+                for (x = 0; x < bitmap.Size.Width; x++)
+                {
+
+                    Color pixelColor = bitmap.GetPixel(x, y);
+
+
+                    if (pixelColor.ToString() == "Color [A=255, R=0, G=0, B=0]")
+                    {
+                        //blackpixels++;
+
+                        Console.WriteLine($"{x} , {y}");
+
+                        SendCmd($"touchHold {(x * 3) + 160} {(y * 3) + 190} 33");
+                        await Task.Delay(160);
+                    }
+                    if (x == 319 && y < 117)
+                    {
+                        y++;
+                        x = 0;
+                    }
+                }               
+        }
+
+        private void Connect_Click_1(object sender, EventArgs e)
+        {
+            Connecter();
+
+        }
+
+
+       
+
+        private void DisconnectBtn_Click(object sender, EventArgs e)
+        {
+            _serverSocket.Close();
+            DisconnectBtn.Enabled = false;
+            Connect.Enabled = true;
+            button2.Enabled =false;
+        }
+    }
+}
